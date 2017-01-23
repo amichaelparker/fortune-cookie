a95367979f899d7b63600508a891305a2dd8c6cf
Adam Parker <amichaelparker@gmail.com>
2017-01-23 15:17:21 -0600

    basic fortune functionality

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = ['I see much code in your future',
+                'Consider eating more fortune cookies',
+                'You have tamed the mighty Python, \
+                now you must free it onto the Great Spider\'s Web']
+    return fortunes[random.randint(0, len(fortunes) - 1)]
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        header = "<h1>Fortune Cookie</h1>"
+        fortune = '<p>Your Fortune: <strong>' + getRandomFortune() + '</strong></p>'
+        lucky_number = '<strong>' + str(random.randint(1, 100)) + '</strong>'
+        number_sentence = '<p>Your lucky number: ' + str(lucky_number) + '</p>'
+        cookie_again_button = '<a href="."><button>Another cookie please!</button></a>'
+
+        sentence = header + fortune + number_sentence + cookie_again_button
+
+        self.response.write(sentence)
+
+routes = [('/', MainHandler)]
+
+app = webapp2.WSGIApplication(routes, debug=True)
